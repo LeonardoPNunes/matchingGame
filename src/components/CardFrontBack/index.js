8656e1d70a722ed81c02da4d762dc1850770961b
Leonardo Nunes <zleo465@gmail.com>
2022-01-22 16:31:00 -0200

    Add the CardGame with javascript icon

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -3,8 +3,9 @@ import CardGame from "../CardGame"
 const CardFrontBack = () =>{
     return/*html*/`
     <article class="Card-front-back">
-       ${CardGame()}
-       ${CardGame()}
+       ${CardGame("javascript","logo do javascript")}
+       ${CardGame("html","logo do html")}
+       ${CardGame("css","logo do css")}
     </article>
     `
 }
